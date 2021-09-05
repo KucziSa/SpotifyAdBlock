ed4f4498030fb71d4220f6c47b3b967d0824a645
Hipolit <76755841+1hipo1@users.noreply.github.com>
2021-09-05 21:16:08 +0200

    Delete script.js

--- Spotify  Adblock/popup/script.js
+++
@@ -1,7 +0,0 @@
-chrome.browserAction.getBadgeText({}, function(result)
-{
-    if (result != "" && result != undefined)
-    {
-        document.getElementById("text").innerText = "So far, " + result + " audio ads were removed successfully in Spotify's queue.";
-    }
-});
